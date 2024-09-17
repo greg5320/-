51ef80f640a65bb3d71b3f5ad861e78e15969a92
greg5320 <art3m43@gmail.com>
2024-09-17 19:26:15 +0300

    deleted add to cart

--- bmstu_lab/urls.py
+++ bmstu_lab/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_maps, name='maps'),
     path('map/<int:id>/', views.get_map_detail, name='map_detail'),
-    path('add_to_cart/<int:map_id>/', views.add_to_cart, name='add_to_cart'),
+
     path('cart/', views.view_cart, name='view_cart'),
 ]
 
