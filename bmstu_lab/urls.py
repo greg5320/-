5a658b19a3ab6acd481c27af87e38b67983aa822
greg5320 <art3m43@gmail.com>
2024-09-23 15:09:46 +0300

    Add .idea to gitignore

--- bmstu_lab/urls.py
+++ bmstu_lab/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_maps, name='maps'),
     path('map/<int:id>/', views.get_map_detail, name='map_detail'),
-
     path('cart/', views.view_cart, name='view_cart'),
 ]
 
