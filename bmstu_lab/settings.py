700ccb9837efa7c886064048bea13570166ef453
greg5320 <art3m43@gmail.com>
2024-11-11 16:42:28 +0300

    feature: add cors unblock for testing

--- bmstu_lab/settings.py
+++ bmstu_lab/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MINIO_STORAGE_ENDPOINT = 'localhost:9000'
@@ -56,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 # SESSION_ENGINE = 'django.contrib.sessions.backends.db'
